8aaebad3128b41d87c3ec9a869071072b600c165
Beorlor <jbdurand2000@gmail.com>
2024-11-10 17:42:36 +0100

    wordpress

--- requirements/wordpress/tools/create_wordpress.sh
+++ requirements/wordpress/tools/create_wordpress.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Wait for MariaDB to be ready
+until mysqladmin ping -h"$MARIA_HOSTNAME" --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
+
 # Check if wp-config.php already exists
 if [ -f ./wp-config.php ]; then
     echo "WordPress is already set up."
@@ -17,6 +23,16 @@ else
     sed -i "s/database_name_here/$MARIA_DATABASE/g" wp-config-sample.php
     cp wp-config-sample.php wp-config.php
 
+    # Install WordPress
+    wp core install \
+        --url="$DOMAIN_NAME" \
+        --title="Your Site Title" \
+        --admin_user="$WORDPRESS_ADMIN" \
+        --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+        --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+        --skip-email \
+        --allow-root
+
     # Enable Redis caching for WordPress
     wp config set WP_REDIS_HOST redis --allow-root
     wp config set WP_REDIS_PORT 6379 --raw --allow-root
@@ -27,4 +43,5 @@ else
     wp redis enable --allow-root
 fi
 
-exec "$@"
+# Start php-fpm in the foreground
+exec php-fpm7.4 -F
